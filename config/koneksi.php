e7a4b07b223e27f2e3110605ed1746cd79549942
Arito8382 <kontol91929@gmail.com>
2025-12-27 13:45:36 +0700

    Add files via upload

---
+++ config/koneksi.php
@@ -0,0 +1,39 @@
+<?php
+date_default_timezone_set('Asia/Jakarta');
+// Konfigurasi Database
+define('DB_HOST', '0.0.0.0');
+define('DB_USER', 'root');
+define('DB_PASS', 'root');
+define('DB_NAME', 'kehadiran_web');
+
+// Koneksi ke database
+$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+// Cek koneksi
+if (!$conn) {
+    die("Koneksi gagal: " . mysqli_connect_error());
+}
+
+// Set charset
+mysqli_set_charset($conn, "utf8");
+
+// Fungsi untuk membersihkan input
+function clean_input($data) {
+    global $conn;
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    $data = mysqli_real_escape_string($conn, $data);
+    return $data;
+}
+
+// Fungsi untuk format tanggal Indonesia
+function tanggal_indonesia($tanggal) {
+    $bulan = array(
+        1 => 'Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni',
+        'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'
+    );
+    $split = explode('-', $tanggal);
+    return $split[2] . ' ' . $bulan[(int)$split[1]] . ' ' . $split[0];
+}
+?>
